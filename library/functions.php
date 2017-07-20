ff75bcfc1230d6bd0cd441973bfc8e3d8479e80c
brightcolor <info@bright-color.de>
2017-07-20 19:19:51 +0200

    German locale / time format

--- library/functions.php
+++ library/functions.php
@@ -4,10 +4,11 @@
  * @param DateTime $dateTime
  * @return string[]
  */
+setlocale(LC_TIME, "de_DE");
 function createDateTimeInfo(DateTime $dateTime) {
     return [
         'full' => $dateTime->format(DateTime::ISO8601),
-        'date' => $dateTime->format('Y-m-d'),
-        'time' => $dateTime->format('H:i:s'),
+        'date' => $dateTime->format('d.m.Y'),
+        'time' => $dateTime->format('H:i'),
     ];
 }
